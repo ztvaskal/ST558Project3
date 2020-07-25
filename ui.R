e13b0d7418ba57127c04d34da1c002c852c4d480
Zack V <65335513+ztvaskal@users.noreply.github.com>
2020-07-24 21:53:53 -0400

    setting up basic blank dashboard

---
+++ ui.R
@@ -0,0 +1,19 @@
+## ui.R ##
+
+library(shinydashboard)
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Basic dashboard"),
+    dashboardSidebar(),
+    dashboardBody(
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            box(plotOutput("plot1", height = 250)),
+            
+            box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+            )
+        )
+    )
+)
