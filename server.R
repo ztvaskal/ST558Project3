6ed6eb8e763f9d5e9163982c9db0e7f74603b3a8
Zack V <65335513+ztvaskal@users.noreply.github.com>
2020-07-24 23:19:13 -0400

    testing setup and getting comfortable with shiny dashboard

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ server <- function(input, output) {
     histdata <- rnorm(500)
     
     output$plot1 <- renderPlot({
-        data <- histdata[seq_len(input$slider)]
+        data <- histdata[seq_len(input$obs1)]
         hist(data)
     })
 }
--- ui.R
+++ ui.R
@@ -1,19 +1,64 @@
 ## ui.R ##
 
+library(shiny)
 library(shinydashboard)
+library(shinythemes)
+library(shinyWidgets)
 
-ui <- dashboardPage(
-    dashboardHeader(title = "Basic dashboard"),
-    dashboardSidebar(),
+ui <- dashboardPage(skin = "red", 
+    dashboardHeader(title = "ST 558 Project 3 - Zack Vaskalis", titleWidth = 350),
+    ## Sidebar content
+    dashboardSidebar(
+        width = 350,
+        sidebarMenu(
+            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+            menuItem("Information", tabName = "info", icon = icon("info-circle")),
+            menuItem("Data", tabName = "data", icon = icon("database")),
+            menuItem("Widgets", tabName = "widgets", icon = icon("th")),
+            menuItem("Charts", tabName = "charts", icon = icon("bar-chart-o"))
+        )
+    ),
+    ## Body content
     dashboardBody(
-        # Boxes need to be put in a row (or column)
-        fluidRow(
-            box(plotOutput("plot1", height = 250)),
+        tabItems(
+            # First tab content
+            tabItem(tabName = "dashboard",
+                    h2("Dashboard content")
+            ),
             
-            box(
-                title = "Controls",
-                sliderInput("slider", "Number of observations:", 1, 100, 50)
-            )
+            # Second tab content
+            tabItem(tabName = "widgets",
+                    h2("Widgets tab content")
+            ),
+            
+            # Third tab content
+            tabItem(tabName = "info",
+                    box(
+                        title = "Title 5", width = 4, background = "red",
+                        "A box with a solid light-blue background"
+                    ),
+                    box(
+                        title = "Title 6",width = 4, background = "black",
+                        "A box with a solid maroon background"
+                    )
+            ),
+            # Fourth tab content
+            tabItem(tabName = "charts",
+                    fluidRow(
+                        box(title = "Histogram", background = "red", solidHeader = TRUE,
+                            plotOutput("plot1", height = 250)),
+                        
+                        box(
+                            title = "Controls", background = "black",
+                            setSliderColor(c("red"),c(1)),
+                            sliderInput("obs1", "Number of observations:", 1, 100, 50)
+                        )
+                    )
+            ),
+            
+            #Fifth tab content
+            tabItem(tabName = "data",
+                    h2("Data Information goes here"))
         )
     )
 )
