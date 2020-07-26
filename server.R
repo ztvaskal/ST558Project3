e87e542447ef8d5a025d41cd88a676c00597ccbb
Zack V <65335513+ztvaskal@users.noreply.github.com>
2020-07-26 15:57:23 -0400

    more general updates

--- server.R
+++ server.R
@@ -12,10 +12,36 @@ library(colourpicker)
 library(tidyverse)
 library(dplyr)
 library(DT)
+library(psych)
 library(plotly)
 
+## Code here that you only need to evaluate once. This can include reading in data, 
+## creation of functions common to all sessions, and reading of other common r scripts.
 
+## Read in dataset from UCI:
+dataurl <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00519/heart_failure_clinical_records_dataset.csv"
+hfcrRAW <- read_csv(dataurl)
 
+## Rename Variables
+names(hfcrRAW)[1]<-"Age"
+names(hfcrRAW)[2]<-"Anemia"
+names(hfcrRAW)[3]<-"CPK"
+names(hfcrRAW)[4]<-"Diabetes"
+names(hfcrRAW)[5]<-"EF"
+names(hfcrRAW)[6]<-"HighBP"
+names(hfcrRAW)[7]<-"Platlet"
+names(hfcrRAW)[8]<-"SCr"
+names(hfcrRAW)[9]<-"SNa"
+names(hfcrRAW)[10]<-"Sex"
+names(hfcrRAW)[11]<-"Smoke"
+names(hfcrRAW)[12]<-"Time"
+names(hfcrRAW)[13]<-"Target"
+
+## Reorder Variables
+hfcrDATA <- hfcrRAW[,c(1,3,5,7,8,9,12,2,4,6,10,11,13)]
+
+    
+## Server - Code here that can be reactive. Differs for every instance of your app that runs.
 server <- function(input, output, session) {
     # Stock TEMPLATE INFO
     set.seed(122)
@@ -37,30 +63,50 @@ server <- function(input, output, session) {
     uciurl <- a("Center for Machine Learning and Intelligent Systems",
                  href="http://cml.ics.uci.edu/")
     output$UCI_URL <- renderUI({
-        tagList("URL:", uciurl)
+        tagList("", uciurl)
     })
     uciproj <- a("Heart Failure Clinical Records Project Page",
                  href="https://archive.ics.uci.edu/ml/datasets/Heart+failure+clinical+records#")
     output$UCI_PROJ <- renderUI({
-        tagList("URL:", uciproj)
+        tagList("", uciproj)
     })    
     dataurl <- a("Heart Failure Clinical Records Full Dataset",
                  href="https://archive.ics.uci.edu/ml/machine-learning-databases/00519/heart_failure_clinical_records_dataset.csv")
     output$UCI_fullDATA <- renderUI({
-        tagList("URL:", dataurl)
+        tagList("", dataurl)
     })
+    uciarticle <- a("[Web Link]",
+                 href="https://bmcmedinformdecismak.biomedcentral.com/articles/10.1186/s12911-020-1023-5")
+    output$UCI_ARTICLE <- renderUI({
+        tagList("", uciarticle)
+    })  
     
     #Info Table of Dataset variables
     table1 <- 0 
     table1 <- tribble( ~Feature, ~Explanation, ~Measurement, ~Range,
-                       "Age", "Age of the patient", "Years", "[40,...,95]")
+                       "Age", "Age of the patient", "Years", "[40,...,95]",
+                       "Creatinine Phosphokinase (CPK)", "Level of the CPK enzyme in the blood", "mcg/L", "[23,...,7861]",
+                       "Ejection Fraction (EF)", "Percentage of blood leaving the heart at each contraction", "Percentage", "[14,...80]",
+                       "Platelet", "Platelets in the blood", "kilplatelets/mL", "[25.01,...,850.00]",
+                       "Serum Creatinine (SCr)", "Level of creatinine in the blood", "mg/dL", "[0.50,...,9.40]",
+                       "Serum Sodium (SNa)", "Level of sodium in the blood", "mEq/L", "[114,...,148]",
+                       "Time", "Follow-up Period", "Days", "[4,...,285]",
+                       "Anemia", "Decrease of Red Blood Cells or Hemoglobin", "Boolean", "0,1",
+                       "High Blood Pressure (HighBP)", "If Patient has Hypertension", "Boolean", "0,1",
+                       "Diabetes", "If Patient has Diabetes", "Boolean", "0,1",
+                       "Sex", "Woman or Man", "Boolean", "0,1",
+                       "Smoke", "If Patient Smokes", "Boolean", "0,1",
+                       "Target (Death Event)", "If Patient Died during follow-up period", "Boolean", "0,1")
 
     #Output Table1 Information
     output$TABLE1 <- renderTable({
         data=table1
     })
     
-    
-    
+    #Output Table2 Information
+    output$TABLE2 <- DT::renderDataTable({
+        hfcrDATA
+    })
+
     
 }
--- ui.R
+++ ui.R
@@ -13,11 +13,12 @@ library(colourpicker)
 library(tidyverse)
 library(dplyr)
 library(DT)
+library(psych)
 library(plotly)
 
 
 ## UI ##
-ui <- dashboardPage(skin = "red", 
+ui <- dashboardPage(skin = "yellow", 
     dashboardHeader(title = "ST 558 Project 3 - Zack Vaskalis", titleWidth = 350),
     ## Sidebar content
     dashboardSidebar(
@@ -35,20 +36,19 @@ ui <- dashboardPage(skin = "red",
         tabItems(
             # First tab content
             tabItem(tabName = "dashboard",
-                    h2("Project 3 Dashboard"),
                     fluidRow(
                         box(
-                            tags$head(tags$style(HTML("a {color: yellow}"))),
-                            background = "navy", width = 5,
+                            tags$head(tags$style(HTML("a {color: red}"))),
+                            background = "navy", width = 13,
                             htmlOutput("UCI_LOGO"),
-                            span(uiOutput("UCI_URL"), style = "color:yellow")
+                            span(uiOutput("UCI_URL"), style = "color:red")
                         )
                     ),
                     
                     fluidRow(
                         box(
-                            tags$head(tags$style(HTML("a {color: yellow}"))),
-                            title = "Heart Failure Clinical Records Dataset", width = 5,
+                            tags$head(tags$style(HTML("a {color: red}"))),
+                            title = "Heart Failure Clinical Records Dataset", width = 13,
                             background = "navy", 
                             "The data for this project comes from the University of California Irvine (UCI)
                             Machine Learning Repository. The project page can be accessed by clicking the 
@@ -59,31 +59,53 @@ ui <- dashboardPage(skin = "red",
                             dataset, where each patient profile contains 13 clinical features, which will be 
                             described in detail on the Information tab in the left-hand toolbar.  The full
                             dataset can be downloaded directly by clicking the 
-                            following link: ", span(uiOutput("UCI_fullDATA"), style = "color:yellow"),tags$br(),
+                            following link: ", span(uiOutput("UCI_fullDATA"), style = "color:red"),tags$br(),
+                            tags$b("Data Set Information: "),tags$br(),
+                            "A detailed description of the dataset can be found in the following paper: ",tags$br(),
+                            "Davide Chicco, Giuseppe Jurman: Machine learning can predict survival of patients 
+                            with heart failure from serum creatinine and ejection fraction alone. BMC Medical 
+                            Informatics and Decision Making 20, 16 (2020).",
+                            span(uiOutput("UCI_ARTICLE"), style = "color:red"),tags$br()
                             
                         )
                     )
             ),
             
             # Second tab content
-            tabItem(tabName = "explore",
-                    h2("Data Exploration tab content"),
-                    box(
-                        title = "Title 6",width = 5, background = "black",
-                        "A box with a solid black background"
+            tabItem(tabName = "explore", h2("Basic Exploratory Data Analysis: Simple Descriptives & Histograms"),
+                    fluidRow(
+                        box(
+                            title = "Title 6",width = 5, background = "black",
+                            "A box with a solid black background"
+                        )
                     ),
+                    fluidRow(
+                        box(
+                            varSelectInput("variable", "Variable:", hfcrDATA)
+                        )
+                    )
             ),
             
             # Third tab content
-            tabItem(tabName = "info",
-                    h2("Information tab content"),
-                    tableOutput("TABLE1")
-            ),
+            tabItem(tabName = "data",
+                    box(title = "Heart Failure Dataset", width = 13,
+                        "Below is a set of tabs containing: [TAB 1] a table of the 13 patient record 
+                        features/variables included in the full heart failure clinical records dataset.  
+                        The table provides what each feature  is, an explanation of the feature, 
+                        how it is measured, and the range of values. [TAB 2] is a data table containing the 
+                        full dataset so you can explore and see the variables and de-identified patient data.",
+                        tags$br(),
+                        ),
+                    tabsetPanel(type = "tabs",
+                                tabPanel("Variables", tableOutput("TABLE1")),
+                                tabPanel("Dataset", DT::dataTableOutput("TABLE2"))
+                    )
+                    ),
             # Fourth tab content
             tabItem(tabName = "charts",
                     fluidRow(
                         box(title = "Histogram", background = "red", solidHeader = TRUE,
-                            plotOutput("plot1", height = 250)),
+                            plotOutput("plot1", height = 500)),
                         
                         box(
                             title = "Controls", background = "black",
@@ -91,12 +113,25 @@ ui <- dashboardPage(skin = "red",
                             sliderInput("obs1", "Size of Points on Graph:",
                                         min = 1, max = 10, value = 5, step = 1)
                         )
+                    ),
+                    fluidRow(
+                        box(
+                            varSelectInput("variable", "Variable:", hfcrDATA)
+                        )
                     )
             ),
             
             #Fifth tab content
-            tabItem(tabName = "data",
-                    h2("Data goes here")
+            tabItem(tabName = "info",
+                    h2("Info page that describes data and app abilities goes here"),
+                    box(
+                        title = "Data Description", width = 6, background = "navy",
+                        "A box with a solid navy background"
+                    ),
+                    box(
+                        title = "App Abilities", width = 6, background = "purple",
+                        "A box with a solid white background"
+                    ),
             )
         )
     )
